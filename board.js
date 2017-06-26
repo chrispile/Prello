a5fb383677282a8fc7bdf3ed663b755030557816
Christian Pile <pilec@uci.edu>
2017-06-26 13:41:13 -0700

    All existing functionality fully updated to work with API using AJAX calls

--- board.js
+++ board.js
@@ -1,42 +1,46 @@
-var mainList = [
-	{
-		title: 'List One', 
-		cards: [
-			{title: 'Card1', description: 'Description 1', colors:[]},
-			{title: 'Card2', description: 'Description 2', colors:[]},
-			{title: 'Card3', description: 'Description 3', colors:[]},
-			{title: 'Card4', description: 'Description 4', colors:[]},
-		]
-	},
-	{
-		title: 'List Two', 
-		cards: [
-			{title: 'Card1', description: 'Description 1', colors:[]},
-			{title: 'Card2', description: 'Description 2', colors:[]},
+// var mainList = [
+// 	{
+// 		title: 'List One', 
+// 		cards: [
+// 			{title: 'Card1', description: 'Description 1', colors:[]},
+// 			{title: 'Card2', description: 'Description 2', colors:[]},
+// 			{title: 'Card3', description: 'Description 3', colors:[]},
+// 			{title: 'Card4', description: 'Description 4', colors:[]},
+// 		]
+// 	},
+// 	{
+// 		title: 'List Two', 
+// 		cards: [
+// 			{title: 'Card1', description: 'Description 1', colors:[]},
+// 			{title: 'Card2', description: 'Description 2', colors:[]},
 
-		]
-	},
-	{
-		title: 'List Three', 
-		cards: [
-			{title: 'Card1', description: 'Description 1', colors:[]},
-			{title: 'Card2', description: 'Description 2', colors:[]},
-			{title: 'Card3', description: 'Description 3', colors:[]},
-			{title: 'Card4', description: 'Description 4', colors:[]},
-		]
-	}
-]
+// 		]
+// 	},
+// 	{
+// 		title: 'List Three', 
+// 		cards: [
+// 			{title: 'Card1', description: 'Description 1', colors:[]},
+// 			{title: 'Card2', description: 'Description 2', colors:[]},
+// 			{title: 'Card3', description: 'Description 3', colors:[]},
+// 			{title: 'Card4', description: 'Description 4', colors:[]},
+// 		]
+// 	}
+// ]
 
 var lol;
+var mainList;
 $(function() {
 	lol = $('#outerList');
-	loadMainList();
+	loadData();
 
 	$('#listForm').submit(function(e) {
 		event.preventDefault();
+		return false;
 	});
 	$('.cardForm').submit(function(e) {
 		event.preventDefault();
+
+		return false;
 	});
 
 	lol.on('click', '.deleteList', deleteList);
@@ -52,6 +56,18 @@ $(function() {
 	$('#addLabelsButton').click(showLabels);
 	$('#labelDiv ul li').click(addLabels);
 });
+
+var loadData = function() {
+	$.ajax( {
+		url: "http://thiman.me:1337/pilec/list",
+		type: "GET",
+		dataType: "json",
+	})
+	.done(function(json) {
+		mainList = json;
+		loadMainList();
+	});
+}
 
 //Loads all the lists from mainList
 	//Used in "main"
@@ -70,10 +86,12 @@ var updateCardList = function(listIndex, cardsUl) {
 	for(var cardIndex = 0; cardIndex < mainList[listIndex].cards.length; cardIndex++) {
 		var card = cardList[cardIndex];
       	var cardLi = $('<li/>').attr('data-cardindex', cardIndex).attr('data-listindex', listIndex);
-		var colorArray = card.colors;
+		var colorArray = card.labels;
 		$.each(colorArray, function(i, color) {
-			var label = $('<div/>').addClass('labelPreview').css('backgroundColor', color);
-			cardLi.append(label);
+				if(color !="") {
+				var label = $('<div/>').addClass('labelPreview').css('backgroundColor', color);
+				cardLi.append(label);
+			}
 		});
 		var cardTitle = $('<h4>').addClass('cardTitle').html(card.title);
 		cardLi.append(cardTitle);
@@ -92,7 +110,7 @@ var createList = function(listIndex) {
 	var addCardButton = $('<button/>').html('Add a card...').addClass('addCard');
 	var addCardDiv = $('<div/>').addClass('addCardDiv');
 	var cardForm = $('<form/>').addClass('cardForm');
-	var cardInput = $('<input/>').addClass('cardInput').attr('type','text').attr('autocomplete', 'off');
+	var cardInput = $('<input/>').addClass('cardInput').attr('type','text').attr('autocomplete', 'off').attr('name', 'cardSubmitName');
 	var addInput = $('<input/>').attr('type','submit').attr('value','Add').addClass('submitCard');
 	cardForm.append(cardInput).append(addInput);
 	addCardDiv.append(cardForm);
@@ -117,12 +135,17 @@ var submitInput = function() {
 
 //Adds a new list to the data structure, and updates UI
 var addList = function(listTitle) {
-	mainList.push({
+	$.ajax({
+		url: "http://thiman.me:1337/pilec/list",
+		data: {
 		'title': listTitle,
-		cards: [], 
+		},
+		type: "POST",
+	}).done(function(json) {
+		mainList.push(json);
+		var listLi = createList(mainList.length-1);
+		lol.append(listLi);
 	});
-	var listLi = createList(mainList.length-1);
-	lol.append(listLi);
 }
 
 //Shows input box after clicking "Add a card" button
@@ -144,16 +167,27 @@ var submitCard = function(event) {
 	var list = $(cardDiv).parent();
 	var listIndex = $(list).attr('data-listindex');
 	addCard(inputValue, listIndex);
+	return false;
 }
 
 //Adds a new card to the data structure, updates the UI
 var addCard = function(title, listIndex) {
-	mainList[listIndex].cards.push({
-		'title': title,
-		description: '',
-		colors: []
+	var newTitle = title;
+	var listID = mainList[listIndex]._id;
+	$.ajax({
+		url: "http://thiman.me:1337/pilec/list/" + listID + "/card",
+		data: {
+			'title': newTitle,
+			'description': '',
+			labels: [''],
+		},
+		type:"POST",
 	})
-	loadMainList();
+	.done(function(json) {
+		alert('added!');
+		mainList[listIndex] = json;
+		loadMainList();
+	});
 }
 
 //MODAL METHODS
@@ -175,15 +209,22 @@ var hideModal = function(event) {
 	$('#cardModal').css('display', 'none');
 	$('#card').css('display', 'none');
 	$('#labelDiv').css('display', 'none');
-
 }
 
 //Deletes a list from data structure and updates UI
 var deleteList = function(event) {
 	var li = $(this).parent();
 	var listIndex = li.attr('data-listindex');
-	mainList.splice(listIndex, 1);
-	loadMainList();
+	$.ajax({
+		url: "http://thiman.me:1337/pilec/list/" + mainList[listIndex]._id,
+		type: 'DELETE'
+	})
+	.done(function() {
+		alert('deleted!');
+		mainList.splice(listIndex, 1);
+		loadMainList();
+
+	});
 }
 
 //Deletes a card from data structure and updates the UI
@@ -191,11 +232,18 @@ var deleteCard = function(event) {
 	var card = $(this).parent();
 	var listIndex = $(card).attr('data-listindex');
 	var cardIndex = $(card).attr('data-cardindex');
-	mainList[listIndex].cards.splice(cardIndex, 1);
-	var selector = '.list[data-listindex=' + listIndex + '] .cardList';
-	var cardList = $(selector);
-	cardList.html('');
-	updateCardList(listIndex, cardList);
+	var listID = mainList[listIndex]._id;
+	var cardID = mainList[listIndex].cards[cardIndex]._id;
+	$.ajax({
+		url: "http://thiman.me:1337/pilec/list/" + listID + "/card/" + cardID,
+		type: "DELETE"
+	})
+	.done(function(){
+		alert('deleted!');
+		mainList[listIndex].cards.splice(cardIndex, 1);
+		loadMainList();
+		hideModal();
+	})
 }
 
 //Applies css background color to labels in the label chooser based on its data attribute color
@@ -217,23 +265,44 @@ var addLabels = function() {
 	var listIndex = $('#card').attr('data-listindex');
 	var cardIndex = $('#card').attr('data-cardindex');
 	var hasColor = false;
-	var colorArray = mainList[listIndex].cards[cardIndex].colors;
-	var hasColor = $.inArray(color, colorArray);
+	var labelArray = mainList[listIndex].cards[cardIndex].labels;
+	var hasColor = $.inArray(color, labelArray);
 	if(hasColor === -1) {
-		colorArray.push(color);
-		$('#labelDiv').css('display', 'none');
-		updateCardLabels(listIndex, cardIndex);
-		loadMainList();	
+		var listID = mainList[listIndex]._id;
+		var cardID = mainList[listIndex].cards[cardIndex]._id;
+		labelArray.push(color);
+		var card =  mainList[listIndex].cards[cardIndex]
+
+
+		$.ajax({
+			url: "http://thiman.me:1337/pilec/list/" + listID + "/card/" + cardID,
+			data: card,
+			// {
+			// 	"title": card.title,
+			// 	"description": card.description,
+			// 	"labels": card.labels,
+			// 	"_id": card._id,
+			// },
+			type: "PATCH"
+		})
+		.done(function(){
+			alert('added label!');
+			$('#labelDiv').css('display', 'none');
+			updateCardLabels(listIndex, cardIndex);
+			loadMainList();	
+		});
 	}
 }
 
 //Updates HTML to include a color label for a card
 var updateCardLabels = function(listIndex, cardIndex) {
 	$('#labelList').html('');
-	var colorArray = mainList[listIndex].cards[cardIndex].colors;
-	$.each(colorArray, function(i, color) {
-		var label = $('<div/>').addClass('cardLabels').css('backgroundColor', color);
-		label.append('+').css('color', color);
-		$('#labelList').append(label);
+	var labelArray = mainList[listIndex].cards[cardIndex].labels;
+	$.each(labelArray, function(i, color) {
+		if(color!="") {
+			var label = $('<div/>').addClass('cardLabels').css('backgroundColor', color);
+			label.append('+').css('color', color);
+			$('#labelList').append(label);
+		}
 	});
 }
