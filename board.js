85232cd9ea5f371cd652ea8efab25b7feecd5b22
Christian Pile <pilec@uci.edu>
2017-06-25 22:45:29 -0700

    Add labels function implemented. Each label added updates on mini card and on card modal

--- board.js
+++ board.js
@@ -2,27 +2,27 @@ var mainList = [
 	{
 		title: 'List One', 
 		cards: [
-			{title: 'Card1', description: 'Description 1'},
-			{title: 'Card2', description: 'Description 2'},
-			{title: 'Card3', description: 'Description 3'},
-			{title: 'Card4', description: 'Description 4'},
+			{title: 'Card1', description: 'Description 1', colors:[]},
+			{title: 'Card2', description: 'Description 2', colors:[]},
+			{title: 'Card3', description: 'Description 3', colors:[]},
+			{title: 'Card4', description: 'Description 4', colors:[]},
 		]
 	},
 	{
 		title: 'List Two', 
 		cards: [
-			{title: 'Card1', description: 'Description 1'},
-			{title: 'Card2', description: 'Description 2'},
+			{title: 'Card1', description: 'Description 1', colors:[]},
+			{title: 'Card2', description: 'Description 2', colors:[]},
 
 		]
 	},
 	{
 		title: 'List Three', 
 		cards: [
-			{title: 'Card1', description: 'Description 1'},
-			{title: 'Card2', description: 'Description 2'},
-			{title: 'Card3', description: 'Description 3'},
-			{title: 'Card4', description: 'Description 4'},
+			{title: 'Card1', description: 'Description 1', colors:[]},
+			{title: 'Card2', description: 'Description 2', colors:[]},
+			{title: 'Card3', description: 'Description 3', colors:[]},
+			{title: 'Card4', description: 'Description 4', colors:[]},
 		]
 	}
 ]
@@ -31,20 +31,26 @@ var lol;
 $(function() {
 	lol = $('#outerList');
 	loadMainList();
-	$('#addListSpan').click(showInput);
-	$('#listSubmit').click(submitInput);
+
 	$('#listForm').submit(function(e) {
 		event.preventDefault();
 	});
 	$('.cardForm').submit(function(e) {
 		event.preventDefault();
 	});
+
 	lol.on('click', '.deleteList', deleteList);
 	lol.on('click', '.cardList li', showModal);
 	lol.on('click', '.addCard', showCardInput);
 	lol.on('click', '.submitCard', submitCard);
+
+	$('#addListSpan').click(showInput);
+	$('#listSubmit').click(submitInput);
 	$('#cardModal').click(hideModal);
 	$('#deleteCard').click(deleteCard);
+	$('#labelDiv ul li').each(labelColors);
+	$('#addLabelsButton').click(showLabels);
+	$('#labelDiv ul li').click(addLabels);
 });
 
 //Loads all the lists from mainList
@@ -64,12 +70,19 @@ var updateCardList = function(listIndex, cardsUl) {
 	for(var cardIndex = 0; cardIndex < mainList[listIndex].cards.length; cardIndex++) {
 		var card = cardList[cardIndex];
       	var cardLi = $('<li/>').attr('data-cardindex', cardIndex).attr('data-listindex', listIndex);
+		var colorArray = card.colors;
+		$.each(colorArray, function(i, color) {
+			var label = $('<div/>').addClass('labelPreview').css('backgroundColor', color);
+			cardLi.append(label);
+		});
 		var cardTitle = $('<h4>').addClass('cardTitle').html(card.title);
 		cardLi.append(cardTitle);
 		cardsUl.append(cardLi);
 	}
 }
 
+//Creates all the HTML elements for one list
+	//used in loadMainList, addList
 var createList = function(listIndex) {
 	var listLi = $('<li/>').addClass('list').attr('data-listindex', listIndex);
 	var deleteListButton = $('<div/>').html('&#10006').addClass('deleteList');
@@ -87,12 +100,13 @@ var createList = function(listIndex) {
 	return listLi;
 }
 
-//addListHelpers
+//Shows input box after clicking on the 'Add a list...' button
 var showInput = function() {
 	$('#addListSpan').css("display", "none");
 	$('#addList').css("display", "inline-block");	
 }
 
+//Submits the input for the add a list form
 var submitInput = function() {
 	var inputValue = $('#listInput').val();
 	$('#listInput').val('');
@@ -101,16 +115,17 @@ var submitInput = function() {
 	$('#addList').css("display", "none");
 }
 
+//Adds a new list to the data structure, and updates UI
 var addList = function(listTitle) {
 	mainList.push({
 		'title': listTitle,
-		cards: []
+		cards: [], 
 	});
 	var listLi = createList(mainList.length-1);
 	lol.append(listLi);
 }
 
-//addCardHelpers
+//Shows input box after clicking "Add a card" button
 var showCardInput = function(event) {
 	$(this).css("display", "none");
 	var parent = $(this).parent();
@@ -118,6 +133,7 @@ var showCardInput = function(event) {
 	$(form).css("display", "block");
 }
 
+//Submits the input for the add a card form
 var submitCard = function(event) {
 	var parent = $(this).parent();
 	var input = $(parent).find('> .cardInput');
@@ -130,21 +146,13 @@ var submitCard = function(event) {
 	addCard(inputValue, listIndex);
 }
 
-
+//Adds a new card to the data structure, updates the UI
 var addCard = function(title, listIndex) {
 	mainList[listIndex].cards.push({
 		'title': title,
-		description: ''
+		description: '',
+		colors: []
 	})
-	loadMainList();
-}
-
-var deleteList = function(event) {
-	var li = $(this).parent();
-	var listIndex = li.attr('data-listindex');
-	console.log(mainList);
-	mainList.splice(listIndex, 1);
-	console.log(mainList);
 	loadMainList();
 }
 
@@ -157,14 +165,28 @@ var showModal = function(event) {
 	$('#cardTitle').html(card.title);
 	$('#desc').html(card.description);
 	$('#cardModal').css('display', 'block');
+	$('#card').css('display','block');
 	$('#card').attr('data-cardindex', cardIndex);
 	$('#card').attr('data-listindex', listIndex);
+	updateCardLabels(listIndex, cardIndex);
 }
 
 var hideModal = function(event) {
 	$('#cardModal').css('display', 'none');
+	$('#card').css('display', 'none');
+	$('#labelDiv').css('display', 'none');
+
 }
 
+//Deletes a list from data structure and updates UI
+var deleteList = function(event) {
+	var li = $(this).parent();
+	var listIndex = li.attr('data-listindex');
+	mainList.splice(listIndex, 1);
+	loadMainList();
+}
+
+//Deletes a card from data structure and updates the UI
 var deleteCard = function(event) {
 	var card = $(this).parent();
 	var listIndex = $(card).attr('data-listindex');
@@ -176,3 +198,42 @@ var deleteCard = function(event) {
 	updateCardList(listIndex, cardList);
 }
 
+//Applies css background color to labels in the label chooser based on its data attribute color
+var labelColors = function() {
+	var color = $(this).attr('data-color');
+	$(this).css('backgroundColor', color);
+}
+
+//Shows the label chooser when clicking on the + button
+var showLabels = function() {
+	if($('#labelDiv').css('display') === 'none') {
+		$('#labelDiv').css('display', 'block'); 
+	}
+}
+
+//Adds a label to the card, updates the card modal UI and the main UI
+var addLabels = function() {
+	var color = $(this).attr('data-color');
+	var listIndex = $('#card').attr('data-listindex');
+	var cardIndex = $('#card').attr('data-cardindex');
+	var hasColor = false;
+	var colorArray = mainList[listIndex].cards[cardIndex].colors;
+	var hasColor = $.inArray(color, colorArray);
+	if(hasColor === -1) {
+		colorArray.push(color);
+		$('#labelDiv').css('display', 'none');
+		updateCardLabels(listIndex, cardIndex);
+		loadMainList();	
+	}
+}
+
+//Updates HTML to include a color label for a card
+var updateCardLabels = function(listIndex, cardIndex) {
+	$('#labelList').html('');
+	var colorArray = mainList[listIndex].cards[cardIndex].colors;
+	$.each(colorArray, function(i, color) {
+		var label = $('<div/>').addClass('cardLabels').css('backgroundColor', color);
+		label.append('+').css('color', color);
+		$('#labelList').append(label);
+	});
+}
